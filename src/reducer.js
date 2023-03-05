03d9d374a9b9a05f731e95f3811b7183647592d4
Eric Doering <ericbenjamindoering@gmail.com>
2023-03-05 18:40:59 -0500

    setup of reducer component

---
+++ src/reducer.js
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const INITIAL_STATE = {
+  todos: []
+};
+
+function reducer(state=INITIAL_STATE, action){
+  if (action.type === "ADD_TODO") {
+    return { ...state, todos: [...state.todos, { task: action.task, id: uuidv4() }]}
+  }
+  if (action.type === "UPDATE_TODO") {
+    const todos = state.todos.map(todo => {
+      if (todo.id === action.id){
+        return { ...todo, task: action.updateTask };
+      }
+      return todo
+    });
+    return {
+      ...state, todos
+    }
+  }
+  if (action.type === "REMOVE_TODO"){
+    return { ...state, todos: state.todos.filter(todo => todo.id !== action.id)}
+  }
+  return state;
+}
+
+export default reducer;
